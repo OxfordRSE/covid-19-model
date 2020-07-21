84c10df2d8bd3bd3ccc556e6f2b302b929e9cdec
Graham Lee <graham.lee@cs.ox.ac.uk>
2020-07-21 14:29:03 +0100

    Extract all remaining duplicate gradient calculations

--- model.py
+++ model.py
@@ -117,7 +117,7 @@ def save_png(plt, filename):
     plt.savefig(filename)
 
 # SEIR model differential equations.
-def susceptible_population_gradient(social_distancing_effectiveness, t, t_social_distancing_start, beta, susceptible, infected):
+def susceptible_becoming_exposed(social_distancing_effectiveness, t, t_social_distancing_start, beta, susceptible, infected):
     return -(1-social_distancing_effectiveness*(1 if t >= 7 * t_social_distancing_start else 0)/100)*beta * susceptible * infected
 
 def deriv(x, t, params):
@@ -127,10 +127,12 @@ def deriv(x, t, params):
     beta = params['beta']
     alpha = params['alpha']
     gamma = params['gamma']
-    dsdt = susceptible_population_gradient(u, t, t_social_distancing, beta, s, i)
-    dedt =  -1 * susceptible_population_gradient(u, t, t_social_distancing, beta, s, i) - alpha * e
-    didt = alpha * e - gamma * i
-    drdt =  gamma * i
+    exposed_becoming_infected = alpha * e
+    infected_becoming_recovered = gamma * i
+    dsdt = susceptible_becoming_exposed(u, t, t_social_distancing, beta, s, i)
+    dedt =  -1 * susceptible_becoming_exposed(u, t, t_social_distancing, beta, s, i) - exposed_becoming_infected
+    didt = exposed_becoming_infected - infected_becoming_recovered
+    drdt =  infected_becoming_recovered
     return [dsdt, dedt, didt, drdt]
 
 parameters = simulation_parameters()
